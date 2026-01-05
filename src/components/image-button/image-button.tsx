e678f84e580d12dd57af421cd6762d4f3d6c451e
lorenzo-giovanardi <myp.lore@gmail.com>
2026-01-05 15:18:39 +0100

    feature: dropdown menu part 1/2

--- src/components/image-button/image-button.tsx
+++ src/components/image-button/image-button.tsx
@@ -6,8 +6,9 @@ interface ImageButtonProps {
 }
 
 export const ImageButton = ({ buttonImgSrc, onHoverImgSrc, onClick, className }: ImageButtonProps) => {
+  const cssClass = className ? className + " cursor-pointer" : "cursor-pointer";
   return (
-    <button onClick={onClick} className={className}>
+    <button onClick={onClick} className={cssClass}>
         {buttonImgSrc && <img className="h-12 pixel-art" src={buttonImgSrc} />} {/* modify height it will be a fixed value */}
     </button>
   );
--- src/components/main-menu/main-menu.tsx
+++ src/components/main-menu/main-menu.tsx
@@ -1,11 +1,12 @@
 import { useNavigate } from "@tanstack/react-router";
 import { ImageButton } from "@components/image-button";
+import { HoverMenu } from "../hover-menu";
 
 export const MainMenu = () => {
   const navigate = useNavigate();
   {/* <NavigationMenuLink onClick={() => navigate({ to: "/" })}>Home</NavigationMenuLink> */}
   return (
-    <div className="flex flex-row w-full">
+    <div className="flex flex-row w-full gap-2">
       <ImageButton
         buttonImgSrc="/images/hamburger-menu.png"
         onClick={() => console.log("Menu clicked")}
@@ -13,13 +14,11 @@ export const MainMenu = () => {
       <ImageButton
         buttonImgSrc="/images/home-icon.png"
         onClick={() => navigate({ to: "/" })}
-        className="ml-4"
       />
-      <ImageButton
-        buttonImgSrc="/images/about-icon.png"
-        onClick={() => navigate({ to: "/about" })}
-        className="ml-4"
-      />
+
+      <HoverMenu onClick={() => navigate({ to: "/about" })}/>
+
+      {/* profile button */}
       <ImageButton
         buttonImgSrc="/images/profile-icon.png"
         onClick={() => console.log("Profile clicked")}

00feb0341cc7b7e1d368fcd13a1aca9f29a4babf
lorenzo-giovanardi <myp.lore@gmail.com>
2026-01-08 14:40:28 +0100

    chore: test with sidebar, yet to decide how to proceed

--- src/components/sidebar-menu/sidebar-menu.tsx
+++ src/components/sidebar-menu/sidebar-menu.tsx
@@ -1,14 +1,17 @@
+import { ImageButton } from "../image-button";
+
 interface SidebarProps {
   visible: boolean;
 }
 
 export const SidebarMenu = ({ visible }: SidebarProps) => {
   return (
+    /* transition-all se vuoi fare più transizioni per ogni param che cambia, ad esempio colore */
     <div
       className={`
         h-screen
         bg-neutral-900 text-white
-        transition-all duration-300 ease-in-out
+        transition-width duration-300 ease-in-out
         overflow-hidden
         ${visible ? "w-[250px]" : "w-0"}
       `}
@@ -16,6 +19,12 @@ export const SidebarMenu = ({ visible }: SidebarProps) => {
       <div className="w-[250px] p-4">
         Sidebar
       </div>
+      <ImageButton
+        background="/images/bg-button.png"
+        onClick={() => console.log("Sidebar button clicked")}
+        icon="fa-solid fa-home"
+        text="Dashboard"
+      />
     </div>
   );
 };
